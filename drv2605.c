1ead7eb72674b9ba449c750fbf28a8e152e31729
a6.webm <46289515+a6-webm@users.noreply.github.com>
2025-02-21 12:21:38 +0000

    translate and add DRV2605 library

---
+++ drv2605.c
@@ -0,0 +1,178 @@
+#include "drv2605.h"
+#include <hardware/i2c.h>
+
+Drv2605 drv2605(i2c_inst_t *i2c_inst, u8 addr) {
+  Drv2605 drv;
+  drv.i2c_inst = i2c_inst;
+  drv.addr = addr;
+  return drv;
+}
+
+// TODO error handling
+void drv2605_write_reg(Drv2605 drv, u8 reg, u8 val) {
+  u8 buffer[2] = {reg, val};
+  i2c_write_blocking(drv.i2c_inst, drv.addr, buffer, 2, false);
+}
+
+// TODO error handling
+u8 drv2605_read_reg(Drv2605 drv, u8 reg) {
+  u8 buffer;
+  // TODO why do we write then read and does this work lol?
+  i2c_write_blocking(drv.i2c_inst, drv.addr, &buffer, 1, false);
+  i2c_read_blocking(drv.i2c_inst, drv.addr, &buffer, 1, false);
+  return buffer;
+}
+
+void drv2605_init(Drv2605 drv) {
+  drv2605_write_reg(drv, DRV2605_REG_MODE, 0x00); // out of standby
+
+  drv2605_write_reg(drv, DRV2605_REG_RTPIN,
+                    0x00); // no real-time-playback
+
+  // TODO what does this do
+  drv2605_write_reg(drv, DRV2605_REG_WAVESEQ1, 1); // strong click
+  drv2605_write_reg(drv, DRV2605_REG_WAVESEQ2, 0); // end sequence
+
+  // write_register(drv2605_inst, DRV2605_REG_OVERDRIVE, 0); // no overdrive
+
+  drv2605_write_reg(drv, DRV2605_REG_SUSTAINPOS, 0);
+  drv2605_write_reg(drv, DRV2605_REG_SUSTAINNEG, 0);
+  drv2605_write_reg(drv, DRV2605_REG_BREAK, 0);
+  drv2605_write_reg(drv, DRV2605_REG_AUDIOMAX, 0x64);
+
+  // TODO change to LRA
+  // ERM open loop
+
+  // turn off N_ERM_LRA
+  drv2605_write_reg(drv, DRV2605_REG_FEEDBACK,
+                    drv2605_read_reg(drv, DRV2605_REG_FEEDBACK) & 0x7F);
+  // turn on ERM_OPEN_LOOP
+  drv2605_write_reg(drv, DRV2605_REG_CONTROL3,
+                    drv2605_read_reg(drv, DRV2605_REG_CONTROL3) | 0x20);
+}
+
+/**************************************************************************/
+/*!
+  @brief Select the haptic waveform to use.
+  @param slot The waveform slot to set, from 0 to 7
+  @param w The waveform sequence value, refers to an index in the ROM library.
+
+    Playback starts at slot 0 and continues through to slot 7, stopping if it
+  encounters a value of 0. A list of available waveforms can be found in
+  section 11.2 of the datasheet: http://www.adafruit.com/datasheets/DRV2605.pdf
+*/
+/**************************************************************************/
+// void Adafruit_DRV2605::setWaveform(uint8_t slot, uint8_t w) {
+//   writeRegister8(DRV2605_REG_WAVESEQ1 + slot, w);
+// }
+
+/**************************************************************************/
+/*!
+  @brief Select the waveform library to use.
+  @param lib Library to use, 0 = Empty, 1-5 are ERM, 6 is LRA.
+
+    See section 7.6.4 in the datasheet for more details:
+  http://www.adafruit.com/datasheets/DRV2605.pdf
+*/
+/**************************************************************************/
+// void Adafruit_DRV2605::selectLibrary(uint8_t lib) {
+//   writeRegister8(DRV2605_REG_LIBRARY, lib);
+// }
+
+/**************************************************************************/
+/*!
+  @brief Start playback of the waveforms (start moving!).
+*/
+/**************************************************************************/
+// void Adafruit_DRV2605::go() { writeRegister8(DRV2605_REG_GO, 1); }
+
+/**************************************************************************/
+/*!
+  @brief Stop playback.
+*/
+/**************************************************************************/
+// void Adafruit_DRV2605::stop() { writeRegister8(DRV2605_REG_GO, 0); }
+
+/**************************************************************************/
+/*!
+  @brief Set the device mode.
+  @param mode Mode value, see datasheet section 7.6.2:
+  http://www.adafruit.com/datasheets/DRV2605.pdf
+
+    0: Internal trigger, call go() to start playback\n
+    1: External trigger, rising edge on IN pin starts playback\n
+    2: External trigger, playback follows the state of IN pin\n
+    3: PWM/analog input\n
+    4: Audio\n
+    5: Real-time playback\n
+    6: Diagnostics\n
+    7: Auto calibration
+*/
+/**************************************************************************/
+// void Adafruit_DRV2605::setMode(uint8_t mode) {
+//   writeRegister8(DRV2605_REG_MODE, mode);
+// }
+
+/**************************************************************************/
+/*!
+  @brief Set the realtime value when in RTP mode, used to directly drive the
+  haptic motor.
+  @param rtp 8-bit drive value.
+*/
+/**************************************************************************/
+// void Adafruit_DRV2605::setRealtimeValue(uint8_t rtp) {
+//   writeRegister8(DRV2605_REG_RTPIN, rtp);
+// }
+
+/**************************************************************************/
+/*!
+  @brief Use ERM (Eccentric Rotating Mass) mode.
+*/
+/**************************************************************************/
+// void Adafruit_DRV2605::useERM() {
+//   writeRegister8(DRV2605_REG_FEEDBACK,
+//                  readRegister8(DRV2605_REG_FEEDBACK) & 0x7F);
+// }
+
+/**************************************************************************/
+/*!
+  @brief Use LRA (Linear Resonance Actuator) mode.
+*/
+/**************************************************************************/
+// void Adafruit_DRV2605::useLRA() {
+//   writeRegister8(DRV2605_REG_FEEDBACK,
+//                  readRegister8(DRV2605_REG_FEEDBACK) | 0x80);
+// }
+
+/**************************************************************************/
+/* Portions of this project have been copied from the Adafruit DRV2605L Haptic
+ * Driver library, which is under the following license */
+/**************************************************************************/
+/*!
+ * @file Adafruit_DRV2605.cpp
+ *
+ * @mainpage Adafruit DRV2605L Haptic Driver
+ *
+ * @section intro_sec Introduction
+ *
+ * This is a library for the Adafruit DRV2605L Haptic Driver ---->
+ * http://www.adafruit.com/products/2305
+ *
+ * Check out the links above for our tutorials and wiring diagrams.
+ *
+ * This motor/haptic driver uses I2C to communicate.
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ * @section author Author
+ *
+ * Written by Limor Fried/Ladyada for Adafruit Industries.
+ *
+ * @section license License
+ *
+ * MIT license, all text above must be included in any redistribution.
+ *
+ */
+/**************************************************************************/
--- tactom-firmware.c
+++ tactom-firmware.c
@@ -1,42 +1,65 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/i2c.h"
+#include "drv2605.h"
 #include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define I2C0_SDA 1
+#define I2C0_SCL 2
+#define I2C1_SDA 4
+#define I2C1_SCL 5
+#define BAUD 400 * 1000
 
+#define NUM_DRV 12
+// I2C1_SPLIT splits the DRV_ADDRESSES array in two, so that the former
+// addresses are accessed with i2c0, and the latter i2c1
+#define I2C1_SPLIT 6
+const u8 DRV_ADDRESSES[NUM_DRV] = {
+    0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75,
+};
 
+int main() {
+  stdio_init_all();
 
+  // Initialise the Wi-Fi chip
+  if (cyw43_arch_init()) {
+    printf("Wi-Fi init failed\n");
+    return -1;
+  }
 
-int main()
-{
-    stdio_init_all();
+  // I2C Initialisation. Using it at 400Khz.
+  if (i2c_init(i2c0, BAUD) != BAUD) {
+    return -1;
+  }
+  if (i2c_init(i2c1, BAUD) != BAUD) {
+    return -1;
+  }
 
-    // Initialise the Wi-Fi chip
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed\n");
-        return -1;
-    }
+  gpio_set_function(I2C0_SDA, GPIO_FUNC_I2C);
+  gpio_set_function(I2C0_SCL, GPIO_FUNC_I2C);
+  gpio_set_function(I2C1_SDA, GPIO_FUNC_I2C);
+  gpio_set_function(I2C1_SCL, GPIO_FUNC_I2C);
+  gpio_pull_up(I2C0_SDA);
+  gpio_pull_up(I2C0_SCL);
+  gpio_pull_up(I2C1_SDA);
+  gpio_pull_up(I2C1_SCL);
 
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+  Drv2605 drvs[NUM_DRV];
+  for (int i = 0; i < I2C1_SPLIT; i++) {
+    drvs[i] = drv2605(i2c0, DRV_ADDRESSES[i]);
+  }
+  for (int i = I2C1_SPLIT; i < NUM_DRV; i++) {
+    drvs[i] = drv2605(i2c1, DRV_ADDRESSES[i]);
+  }
 
-    // Example to turn on the Pico W LED
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+  // Example to turn on the Pico W LED
+  // cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+  for (int i = 0; i < NUM_DRV; i++) {
+    drv2605_init(drvs[i]);
+  }
+
+  while (true) {
+    printf("Hello, world!\n");
+    sleep_ms(1000);
+  }
 }
